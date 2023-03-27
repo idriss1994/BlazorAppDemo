96c142df32ab7010f379088ba835cd9a044840b4
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-03-27 23:49:41 +0100

    PizzaService

--- BlazorAppDemo/Data/Pizza.cs
+++ BlazorAppDemo/Data/Pizza.cs
@@ -7,6 +7,7 @@
         public string Name { get; set; } = String.Empty;
 
         public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
 
         public decimal Price { get; set; }
 
---
+++ BlazorAppDemo/Data/PizzaService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorAppDemo.Data
+{
+    public class PizzaService
+    {
+        public async Task<List<Pizza>> GetPizzasAsync()
+        {
+           return new List<Pizza>
+           {
+                new Pizza { Name = "The Baconatorizor", Price =  11.99M, Description = "It has EVERY kind of bacon", ImageUrl="img/pizzas/bacon.jpg"},
+                new Pizza { Name = "Buffalo chicken", Price =  12.75M, Description = "Spicy chicken, hot sauce, and blue cheese, guaranteed to warm you up", ImageUrl="img/pizzas/meaty.jpg"},
+                new Pizza { Name = "Veggie Delight", Price =  11.5M, Description = "It's like salad, but on a pizza", ImageUrl="img/pizzas/salad.jpg"},
+                new Pizza { Name = "Margherita", Price =  9.99M, Description = "Traditional Italian pizza with tomatoes and basil", ImageUrl="img/pizzas/margherita.jpg"},
+                new Pizza { Name = "Basic Cheese Pizza", Price =  11.99M, Description = "It's cheesy and delicious. Why wouldn't you want one?", ImageUrl="img/pizzas/cheese.jpg"},
+                new Pizza { Name = "Classic pepperoni", Price =  10.5M, Description = "It's the pizza you grew up with, but Blazing hot!", ImageUrl="img/pizzas/pepperoni.jpg" }
+           };
+        }
+    }
+}
