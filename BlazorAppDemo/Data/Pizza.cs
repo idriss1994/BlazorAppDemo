197a27947cebf4703974b1e4711c6500e6e66873
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-03-27 23:43:36 +0100

    Adding Pizza model

---
+++ BlazorAppDemo/Data/Pizza.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorAppDemo.Data
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+
+        public string Name { get; set; } = String.Empty;
+
+        public string Description { get; set; } = String.Empty;
+
+        public decimal Price { get; set; }
+
+        public bool Vegetarian { get; set; }
+
+        public bool Vegan { get; set; }
+    }
+}
