461647b6193cfc9cd7278acef9d2f9d90a2144ec
IDRISS AIT SI EL ARABI <idriss199442@gmail.com>
2023-04-19 15:07:05 +0000

    Add DisplayPizza component Add PizzaTopping model using Component parameter to pass data to child component

---
+++ BlazorAppDemo/Components/DisplayPizza.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorAppDemo.Models.Topping;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppDemo.Components
+{
+    public partial class DisplayPizza
+    {
+        [Parameter]
+        public string PizzaName { get; set; } = string.Empty;
+
+        [Parameter]
+        public string PizzaDescription { get; set; } = "The best pizza you've ever teested";
+
+        [Parameter]
+        public PizzaTopping Topping { get; set; }
+    }
+}
---
+++ BlazorAppDemo/Models/Topping/PizzaTopping.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAppDemo.Models.Topping
+{
+    public class PizzaTopping
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Ingredients { get; set; } = string.Empty;
+    }
+}
